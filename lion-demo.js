91979200e63b0322167271cd3afe1faa2c2421e5
tlouisse <t_louisse@hotmail.com>
2024-11-28 09:27:10 +0100

    yes

--- lion-demo.js
+++ lion-demo.js
@@ -1,25 +1,45 @@
-import { html, LitElement } from 'lit';
+import { LitElement, html, css } from 'lit';
 
 // As a side-effect this way of importing defines the custom elements, eg. <lion-button>, ready for use
 import '@lion/ui/define/lion-button.js';
-import '@lion/ui/define/lion-tooltip.js';
+import '@lion/ui/define/lion-dialog.js';
+
+class ContentNodeWithShadowRoot extends LitElement {
+  static styles = [
+    css`
+    :host { 
+      background-color: white;
+      padding: 1rem;
+    }`
+  ];
+
+  render() {
+    return html`<slot></slot>`;
+  }
+}
+customElements.define('content-node-with-shadow-root', ContentNodeWithShadowRoot);
 
 export class LionDemo extends LitElement {
   static properties = {
-    header: { type: String },
     counter: { type: Number },
+    header: { type: String },
   };
+
   constructor() {
     super();
     this.header = 'Hey dev';
     this.counter = 0;
   }
+  
   render() {
     return html`
-      <h1>${this.header}! Increment is at Nr: ${this.counter}</h1>
-      <lion-tooltip has-arrow>
-        <lion-button slot="invoker" @click=${() => {this.counter += 1; console.log(this.counter);}}>increment</lion-button>
-        <span slot="content"> +1 </span>
+      <lion-dialog>
+        <lion-button slot="invoker">open</lion-button>
+        <content-node-with-shadow-root slot="content">
+          bla bla bla 
+
+          <input> <lion-button>sadasd</lion-button>
+        </content-node-with-shadow-root>
       </lion-tooltip>
     `;
   }
