4f5c6774b09dfd9747daa4c55ccf6131f14ba290
tlouisse <t_louisse@hotmail.com>
2025-09-16 22:36:38 +0200

    hm hm

--- lion-demo.js
+++ lion-demo.js
@@ -1,26 +1,61 @@
 import { html, LitElement } from 'lit';
 
+import { LionDialog } from '@lion/ui/dialog.js';
+import { OverlayController } from '@lion/ui/overlays.js';
+
+const originalHide = OverlayController.prototype.hide;
+OverlayController.prototype.hide = function(...args) {
+  console.debug('before hide, body margin:', document.documentElement.style.marginRight);
+  originalHide.call(this, ...args);
+  console.debug('hidden, body margin:', document.documentElement.style.marginRight);
+}
+
 // As a side-effect this way of importing defines the custom elements, eg. <lion-button>, ready for use
-import '@lion/ui/define/lion-button.js';
-import '@lion/ui/define/lion-tooltip.js';
+// import '@lion/ui/define/lion-dialog.js';
+
+class DebugDialog extends LionDialog {
+  /**
+   * @overridable
+   * @desc use this method to setup your open and close event listeners
+   * For example, set a click event listener on _overlayInvokerNode to set opened to true
+   * @protected
+   */
+  // eslint-disable-next-line class-methods-use-this
+  _setupOpenCloseListeners() {
+    /**
+     * @param {{ stopPropagation: () => void; }} ev
+     */
+    this.__closeEventInContentNodeHandler = ev => {
+      console.debug('handlet evt')
+      ev.stopPropagation();
+      /** @type {OverlayController} */ (this._overlayCtrl).hide();
+    };
+
+    if (this._overlayContentNode) {
+      console.debug('registreert evt');
+      this._overlayContentNode.addEventListener(
+        'close-overlay',
+        this.__closeEventInContentNodeHandler,
+      );
+    }
+  }
+}
+customElements.define('lion-dialog', DebugDialog);
+
+
 
 export class LionDemo extends LitElement {
-  static properties = {
-    header: { type: String },
-    counter: { type: Number },
-  };
-  constructor() {
-    super();
-    this.header = 'Hey dev';
-    this.counter = 0;
-  }
   render() {
     return html`
-      <h1>${this.header}! Increment is at Nr: ${this.counter}</h1>
-      <lion-tooltip has-arrow>
-        <lion-button slot="invoker" @click=${() => {this.counter += 1; console.log(this.counter);}}>increment</lion-button>
-        <span slot="content"> +1 </span>
-      </lion-tooltip>
+      <lion-dialog>
+        <button type="button" slot="invoker">open</button>
+        <div slot="content" style="background-color: white;">
+          zeer content
+          <button type="button" @click="${({currentTarget}) => currentTarget.dispatchEvent(new Event('close-overlay', { bubbles: true }))}">
+            close
+          </button>
+        </div>
+      </lion-dialog>
     `;
   }
 }
